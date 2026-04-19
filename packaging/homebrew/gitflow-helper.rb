2a4f3856befce1ba9598276125c937061c5578ce
Luis R. Lozano <8082703+llozanos81@users.noreply.github.com>
2026-04-19 17:08:20 -0600

    squash(feature): remove-goreleaser-deprecations

--- packaging/homebrew/gitflow-helper.rb
+++ packaging/homebrew/gitflow-helper.rb
@@ -4,16 +4,16 @@
 class GitflowHelper < Formula
   desc "Git Flow workflow helper — interactive TUI + CLI. Only requires git."
   homepage "https://github.com/novaemx/gitflow-helper"
-  version "0.5.38"
+  version "0.5.39"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/novaemx/gitflow-helper/releases/download/v0.5.38/gitflow-0.5.38-darwin-universal.tar.gz"
-      sha256 "42364615724999bd3c2b20495011956bc4a2fd15d193702dfbaf12a07db08c88"
+      url "https://github.com/novaemx/gitflow-helper/releases/download/v0.5.39/gitflow-0.5.39-darwin-universal.tar.gz"
+      sha256 "532fbedc51c783389447fda28e1256074d4408dc0b6349205143e1a1ac01469e"
     else
-      url "https://github.com/novaemx/gitflow-helper/releases/download/v0.5.38/gitflow-0.5.38-darwin-universal.tar.gz"
-      sha256 "42364615724999bd3c2b20495011956bc4a2fd15d193702dfbaf12a07db08c88"
+      url "https://github.com/novaemx/gitflow-helper/releases/download/v0.5.39/gitflow-0.5.39-darwin-universal.tar.gz"
+      sha256 "532fbedc51c783389447fda28e1256074d4408dc0b6349205143e1a1ac01469e"
     end
   end
 
