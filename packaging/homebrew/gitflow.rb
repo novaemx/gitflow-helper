e97aaa48e9fb42bb80d66faf2f4339696019619a
Luis R. Lozano <8082703+llozanos81@users.noreply.github.com>
2026-04-30 19:58:15 -0600

    chore(release): align 0.5.52 asset checksums

--- packaging/homebrew/gitflow.rb
+++ packaging/homebrew/gitflow.rb
@@ -3,7 +3,7 @@ class Gitflow < Formula
   homepage "https://github.com/novaemx/gitflow-helper"
   url "https://github.com/novaemx/gitflow-helper/releases/download/v0.5.52/gitflow-0.5.52-darwin-universal.tar.gz"
   version "0.5.52"
-  sha256 "61f92f4e97de2bc757cad2f637e91b00e012be3bc98ea0522500f347863d429f"
+  sha256 "f3f99f50475311e134ad306ed7feee66d941f3f02157d350b0dbc1449f3e5ae9"
   license "MIT"
 
   def install
